67bf41bbf921e55ad48019ce113e1c9b4c9215b4
KwisatzX <kwisatz4@gmail.com>
2022-01-24 10:17:41 +0100

    Initial commit

---
+++ src/main/java/io/github/kwisatzx/klgtask/KlgRecruitmentTaskApplication.java
@@ -0,0 +1,13 @@
+package io.github.kwisatzx.klgtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlgRecruitmentTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KlgRecruitmentTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/kwisatzx/klgtask/KlgRecruitmentTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kwisatzx.klgtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlgRecruitmentTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
