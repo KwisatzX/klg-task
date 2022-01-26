4ad0452c6e10a4a72af48929509f5a079e8aa033
KwisatzX <kwisatz4@gmail.com>
2022-01-26 11:53:04 +0100

    Removed unneeded file

--- src/test/java/io/github/kwisatzx/klgtask/controllers/ReservationRestControllerTest.java
+++ src/test/java/io/github/kwisatzx/klgtask/controllers/ReservationRestControllerTest.java
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class ReservationRestControllerTest {
+class RentalPropertyRestController {
 
     @Autowired
     private MockMvc mockMvc;
